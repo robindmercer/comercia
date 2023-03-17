61b9f73ff0ef97b63394bc8a9435c117251dc704
robindmercer <robindmercer@gmail.com>
2023-03-17 17:07:56 -0300

    deply 5

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,5 @@
 import { createStore, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
-import rootReducer from '../Reducer'
+import rootReducer from '../Reducer/index'
  
 export const store = createStore(rootReducer, compose(applyMiddleware(thunk)))
