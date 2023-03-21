de5b538b61b70775bc5d9e10fe57fcf5ea63604d
robindmercer <robindmercer@gmail.com>
2023-03-21 18:16:24 -0300

    Ventas

---
+++ client/src/actions/factdet.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { GET_FACTURADET } from './constant'
+const url = "http://localhost:3001"
+
+ export function getFacturaDet(id){
+  console.log('Action getFactura: DET');
+   return async function (dispatch) {
+       var factdet = await axios.get(`/factdet/det?id=${id}`);
+       return dispatch({
+           type: GET_FACTURADET,
+           payload: factdet.data
+       })
+   }
+}
+
+export function AddFacturaDet(input) {
+    console.log('AddFacturaDet input: ', input);
+    return function (dispatch) {
+      axios.post(`${url}/factdet`, input)
+        .then(response => {
+            console.log('AddFacturaDet response: ', response);
+          return response
+        })
+        .catch(err => {
+          console.log('Error:',err)
+        })
+    }
+  }
+  
---
+++ client/src/actions/factura.js
@@ -0,0 +1,114 @@
+// Reemplar FACTURA Factura factura por la tabla a crear 
+import axios from "axios";
+import { GET_FACTURA, GET_FACTCAB, RESET_FAC } from './constant'
+import { AddFacturaDet } from "./factdet";
+
+const url = "http://localhost:3001"
+
+export function getFactura() {
+  console.log('Action getFactura: All');
+  return async function (dispatch) {
+    var factura = await axios.get(`${url}/factura`);
+    return dispatch({
+      type: GET_FACTURA,
+      payload: factura.data
+    })
+  }
+}
+
+export function getFacturaCab(id) {
+  console.log('Action getFacturaCab: ', id);
+  return async function (dispatch) {
+    var factura = await axios.get(`${url}/factura/Cab?id=${id}`);
+    return dispatch({
+      type: GET_FACTCAB,
+      payload: factura.data
+    })
+  }
+}
+
+export function postFacturaNew(factcab) {
+  console.log('Action putFacturaNew: ', factcab);
+  return async function (dispatch) {
+    var factura = await axios.post(`${url}/factura`, factcab);
+    console.log('factura: ', factura);
+    return dispatch({
+      type: GET_FACTCAB,
+      payload: factura
+    })
+  }
+}
+
+export function AddFactura(factcab, factdet, inputDet) {
+  return function (dispatch) {
+    axios.post(`${url}/factura`, factcab)
+      .then(response => {
+        var xOrden = 0;
+        // console.log('response: ', response.data);
+        // console.log('response: ', response.data[0][0].id);
+        factdet.forEach((fact) => {
+          xOrden += 1
+          inputDet.fac_id = response.data[0][0].id
+          inputDet.orden = xOrden
+          inputDet.prod_id = fact.prod_id
+          inputDet.precio = fact.precio
+          inputDet.cantidad = fact.cantidad
+          inputDet.total = fact.total
+          dispatch(AddFacturaDet(inputDet))
+          //console.log('factDet ', inputDet);
+        })
+        return response
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+}
+
+export function UpdateFactura(factcab, factdet, inputDet) {
+  return function (dispatch) {
+    console.log('UpdateFactura ', factcab);
+    axios.put(`${url}/factura`, factcab)
+      .then(response => {
+        if (response.data != 'OK') {
+          console.log('response: ', response);
+          alert(response.data);
+          return "Err";
+        } else {
+          console.log('UpdateFactura response: ', response);
+          var xOrden = 0;
+          factdet.forEach((fact) => {
+            xOrden += 1
+            inputDet.fac_id = fact.fac_id
+            inputDet.orden = xOrden
+            inputDet.prod_id = fact.prod_id
+            inputDet.precio = fact.precio
+            inputDet.cantidad = fact.cantidad
+            inputDet.total = fact.total
+            dispatch(AddFacturaDet(inputDet))
+          })
+          return response
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+}
+
+export function UpdateFacturaSts(id, sts) {
+  return async function (dispatch) {
+    var factura = await axios.put(`${url}/factura/stat?id=${id}&sts=${sts}`);
+    return dispatch({
+      type: GET_FACTCAB,
+      payload: factura.data
+    })
+  }
+}
+
+
+export function resetFact() {
+  return {
+    type: RESET_FAC
+  }
+}
