c00e39a64ed1ae03c24244fb6304927c14b14c72
robindmercer <robindmercer@gmail.com>
2023-03-21 18:20:26 -0300

    Ventas

--- client/src/actions/factdet.js
+++ client/src/actions/factdet.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 import { GET_FACTURADET } from './constant'
-//const url = "http://localhost:3001"
 
  export function getFacturaDet(id){
   console.log('Action getFactura: DET');
    return async function (dispatch) {
-       var factdet = await axios.get(`/factdet/det?id=${id}`);
+       var factdet = await axios.get(`factdet/det?id=${id}`);
        return dispatch({
            type: GET_FACTURADET,
            payload: factdet.data
@@ -16,7 +15,7 @@ import { GET_FACTURADET } from './constant'
 export function AddFacturaDet(input) {
     console.log('AddFacturaDet input: ', input);
     return function (dispatch) {
-      axios.post(`${url}/factdet`, input)
+      axios.post(`factdet`, input)
         .then(response => {
             console.log('AddFacturaDet response: ', response);
           return response
