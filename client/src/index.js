964dabb291999fad88f4bfbf76b2e0e3d25e260a
robindmercer <robindmercer@gmail.com>
2023-04-20 10:58:15 -0300

    Conection RailwWay

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux'
 import { store } from './store/index'
 import axios from 'axios'
 //axios.defaults.baseURL = 'http://localhost:3001'
-axios.defaults.baseURL = 'https://comercia-production.up.railway.app'
+axios.defaults.baseURL = 'https://comercia-production.up.railway.app/'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
