54dfb63f3537f399db87cef9e590c689af6d88de
robindmercer <robindmercer@gmail.com>
2023-04-21 13:44:22 -0300

    Ventas 1.9

--- client/src/components/admin/productoLang.js
+++ client/src/components/admin/productoLang.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getProductoLang } from "../../actions/productolang";
+import { getProductoLang } from "../../actions/productoLang";
 import { Link } from "react-router-dom";
 import '../../css/producto.css'
 import Header from '../Header';
