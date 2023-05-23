d22154e1d7dfb2d0d45ab8ca014522677f7e97c8
robindmercer <robindmercer@gmail.com>
2023-05-23 13:31:55 -0300

    PDF Creation version 2

--- api/index.js
+++ api/index.js
@@ -6,9 +6,9 @@ const { conn } = require('./src/db.js');
 const refLoad= require('./src/loader/reference.js');
 const port = process.env.PORT ||3001
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  refLoad()
-  // conn.sync().then(() => {
+// conn.sync({ force: true }).then(() => {
+//   refLoad()
+  conn.sync().then(() => {
   server.listen(port, () => {
     console.log(`Server listening at ${port}`); // eslint-disable-line no-console
   });
