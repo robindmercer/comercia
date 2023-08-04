2a0b4e0b199e5c1591358ec6d120405c3e6a7a2b
robindmercer <robindmercer@gmail.com>
2023-08-04 09:47:34 -0300

    Seleccion de Clientes

--- client/src/components/ventas/cotizAsignClient.js
+++ client/src/components/ventas/cotizAsignClient.js
@@ -4,97 +4,89 @@ import { useDispatch, useSelector } from "react-redux";
 import { getCliente, getClienteByName } from "../../actions/cliente";
 import { cotiToFact } from "../../actions/factura";
 
-import "../admin/condiciones/add.css"
+import "../admin/condiciones/add.css";
 
 const AsignCli = (cotid) => {
-  // const { addCondicion } = useContext(CondicionContext);
-  const [cliid, setCliid] = useState(0);
-  const [onChange, setOnChange] = useState(false);
-  const [ver,setVer] = useState(false)
-  const { cliente } = useSelector((state) => state);
-  const [nombre, setName] = useState("");
-  const dispatch = useDispatch();
+   // const { addCondicion } = useContext(CondicionContext);
+   const [cliid, setCliid] = useState(0);
+   const [onChange, setOnChange] = useState(false);
+   const [ver, setVer] = useState(false);
+   const { cliente } = useSelector((state) => state);
+   const [nombre, setName] = useState("");
+   const dispatch = useDispatch();
 
-  const [input, setInput] = useState({
-    cli_id: '',
-    cot_id: 0,
-  });
+   const [input, setInput] = useState({
+      cli_id: "",
+      cot_id: 0,
+   });
 
-  useEffect(() => {
-    dispatch(getCliente());
-}, [dispatch]);
+   useEffect(() => {
+      dispatch(getCliente());
+   }, [dispatch]);
 
-  function handlePerfil(e) {
-    e.preventDefault();
-    console.log('e.target.value: ', e.target.value);
-    setCliid(e.target.value)
-    if (onChange) {
-      setOnChange(false);
-    } else {
-      setOnChange(true);
-    }    
-    console.log('cliid: ', cliid);
-  }
+   function handlePerfil(e) {
+      e.preventDefault();
+      console.log("e.target.value: ", e.target.value);
+      setCliid(e.target.value);
+      if (onChange) {
+         setOnChange(false);
+      } else {
+         setOnChange(true);
+      }
+      console.log("cliid: ", cliid);
+   }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setVer(false)
-    if (cliid === 0 || cotid===0){
-      setVer(true)
-    } else {
-    setInput(input.cli_id = cliid)
-    setInput(input.cot_id = cotid.cotid)
-    dispatch(cotiToFact(input));
-    //console.log('input: ', input);
-    window.location.href = '/factura';
-    }
-  };
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      setVer(false);
+      if (cliid === 0 || cotid === 0) {
+         setVer(true);
+      } else {
+         setInput((input.cli_id = cliid));
+         setInput((input.cot_id = cotid.cotid));
+         dispatch(cotiToFact(input));
+         //console.log('input: ', input);
+         window.location.href = "/factura";
+      }
+   };
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <table>
-        {/* <tr  >
-          <td className="td1">Cliente</td>
-          <td>
-            <input
-              className="td2Des"
-              type="text"
-              placeholder="Nombre de Cliente *"
-              name="cli_id"
-              value={input.cli_id}
-              onKeyPress={(e) => handleInputChange(e)}
-              onChange={(e) => setName(e.target.value)}
-              required
-            />
-          </td>
-        </tr> */}
-        <tr>
-          <td>Seleccion</td>
-          <td>
-          <select className="selWidth"
-                  name="cliente"
-                  id="cliente"
-                  onChange={(e) => handlePerfil(e)}
-                  value={input.cod_cliente}
-                >
-                  <option value="0">Seleccionar</option>
-                  {cliente.map((perf) => {
-                    return (
-                          <option
-                            value={perf.id}
-                            key={perf.id}
-                          >{`${perf.nombre}`}</option>                   
-                          )
-                  })}
-                </select>
-          </td>
-        </tr>
-      </table>
-      <Button variant="success" type="submit" block>
-        Asignar al Cliente
-      </Button>
-    </Form>
-  );
+   return (
+      <Form onSubmit={handleSubmit}>
+         <table>
+            <tbody>
+               <tr>
+                  <td>Seleccion</td>
+                  <td>
+                     <select
+                        className="selWidth"
+                        name="cliente"
+                        id="cliente"
+                        onChange={(e) => handlePerfil(e)}
+                        value={input.cod_cliente}
+                     >
+                        <option value="0">Seleccionar</option>
+                        {cliente.map((perf) => {
+                           var nombre = perf.nombre + ' ' + perf.apellido
+                           if (nombre.indexOf(perf.razsoc) < 0){
+                               nombre += ' - ' + perf.razsoc
+                           }
+                           return (
+                              <option
+                                 value={perf.id}
+                                 key={perf.id}
+                              >{`${nombre}`}</option>
+                           );
+                        })}
+                     </select>
+                  </td>
+               </tr>
+            </tbody>
+         </table>
+         <Button variant="success" type="submit" block>
+            Asignar al Cliente
+         </Button>
+      </Form>
+   );
 };
 
 export default AsignCli;
