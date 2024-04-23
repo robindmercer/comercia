446673f507edcfd33000586e82cbf81c02e61450
robindmercer <robindmercer@gmail.com>
2024-04-23 14:19:12 -0300

    Direccion Entrega Cliente

--- client/src/components/ventas/cotizAsignClient.js
+++ client/src/components/ventas/cotizAsignClient.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { getCliente, getClienteByName } from "../../actions/cliente";
+import { getClienteDir } from "../../actions/cliente";
 import { cotiToFact } from "../../actions/factura";
 
 import "../admin/condiciones/add.css";
@@ -14,20 +14,29 @@ const AsignCli = (cotid) => {
    const { cliente } = useSelector((state) => state);
    const [nombre, setName] = useState("");
    const dispatch = useDispatch();
-
+   var ant = "";
    const [input, setInput] = useState({
       cli_id: "",
       cot_id: 0,
+      dir_id: 0,
    });
 
    useEffect(() => {
-      dispatch(getCliente());
+      dispatch(getClienteDir());
    }, [dispatch]);
 
    function handlePerfil(e) {
       e.preventDefault();
-      console.log("e.target.value: ", e.target.value);
-      setCliid(e.target.value);
+      if (e.target.name ==="cliente"){
+         input.cli_id=e.target.value
+         setCliid(e.target.value);
+      }
+      
+      if (e.target.name ==="domic"){
+         input.dir_id=e.target.value
+      }
+      
+      console.log("e.target.value: ", e.target.value, input);
       if (onChange) {
          setOnChange(false);
       } else {
@@ -50,6 +59,8 @@ const AsignCli = (cotid) => {
       }
    };
 
+   console.log('cliente: ', cliente);
+
    return (
       <Form onSubmit={handleSubmit}>
          <table>
@@ -66,20 +77,55 @@ const AsignCli = (cotid) => {
                      >
                         <option value="0">Seleccionar</option>
                         {cliente.map((perf) => {
-                           var nombre = perf.nombre + ' ' + perf.apellido
-                           if (nombre.indexOf(perf.razsoc) < 0){
-                               nombre += ' - ' + perf.razsoc
+                           var nombre = perf.nombre + " " + perf.apellido;
+                           if (nombre.indexOf(perf.razsoc) < 0) {
+                              nombre += " - " + perf.razsoc;
                            }
-                           return (
-                              <option
-                                 value={perf.id}
-                                 key={perf.id}
-                              >{`${nombre}`}</option>
-                           );
+                           if (nombre !== ant) {
+                              ant = nombre;
+                              return (
+                                 <option
+                                    value={perf.id}
+                                    key={perf.id}
+                                 >{`${nombre}`}</option>
+                              );
+                           } else {
+                              return null;
+                           }
                         })}
                      </select>
                   </td>
                </tr>
+               {cliid > 0 ? (
+                  <tr>
+                     <td>Entrega</td>
+                     <td>
+                        <select
+                           className="selWidth"
+                           name="domic"
+                           id="domic"
+                           onChange={(e) => handlePerfil(e)}
+                           value={input.dirId}
+                        >
+                           <option value="0">Seleccionar</option>
+                           {cliente.map((direc) => {
+                              var nombre = 
+                                 direc.dirdes + " : " + direc.cc + " " + direc.dd + " " + direc.cui + " " +direc.dd;
+                              if (parseInt(direc.id) === parseInt(input.cli_id)){
+                              return (
+                                 <option
+                                    value={direc.dirid}
+                                    key={direc.dirid}
+                                 >{`${nombre}`}</option>
+                              );} else{
+                                 return (
+                                    null
+                                 )}
+                           })}
+                        </select>
+                     </td>
+                  </tr>
+               ) : null}
             </tbody>
          </table>
          <Button variant="success" type="submit" block>
