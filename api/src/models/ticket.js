69c80a4badd83cecdbc4d35492e6a3c4c264d0e5
robindmercer <robindmercer@gmail.com>
2024-07-25 11:32:38 -0300

    Table Ticket

--- api/src/models/ticket.js
+++ api/src/models/ticket.js
@@ -14,18 +14,18 @@ module.exports = (sequelize) => {
         porque3:     {type: DataTypes.STRING,  allowNull: false,},
         porque4:     {type: DataTypes.STRING,  allowNull: false,},
         porque5:     {type: DataTypes.STRING,  allowNull: false,},
-        analisis:    {type: DataTypes.STRING,  allowNull: false,},//Conclucion del analisis
-        chkbox:      {type: DataTypes.STRING,  allowNull: false,},// Punto 3,4 y 5 son 9 los check's
-        responsable: {type: DataTypes.STRING,  allowNull: false,},// Nombre del responsable punto 4
-        actividad:   {type: DataTypes.STRING,  allowNull: false,},// Punto 6 Actividad
+        analisis:    {type: DataTypes.STRING,  allowNull: false,}, // Conclucion del analisis
+        chkbox:      {type: DataTypes.STRING,  allowNull: false,}, // Punto 3,4 y 5 son 9 los check's
+        responsable: {type: DataTypes.STRING,  allowNull: false,}, // Nombre del responsable punto 4
+        actividad:   {type: DataTypes.STRING,  allowNull: false,}, // Punto 6 Actividad
         fact:        {type: DataTypes.DATE,    allowNull: false,}, // Fecha ctividad
         evi_act:     {type: DataTypes.TEXT,    allowNull: false,}, // Evidencia Actividad
         conclusion:  {type: DataTypes.TEXT,    allowNull: false,}, // Punto 7
 // Datos propios         
         usr:         {type: DataTypes.STRING,  allowNull: false,},
-        perfil:      {type: DataTypes.INTEGER,  allowNull: false,},// a que departamento 
-        prioridad:   {type: DataTypes.INTEGER, allowNull: false,},// Alta media baja      
-        cod_status:  {type: DataTypes.INTEGER, allowNull: false,},// Abierto Cerrado 
+        perfil:      {type: DataTypes.INTEGER,  allowNull: false,}, // a que departamento 
+        prioridad:   {type: DataTypes.INTEGER, allowNull: false,}, // Alta media baja      
+        cod_status:  {type: DataTypes.INTEGER, allowNull: false,}, // Abierto Cerrado 
         alta:        {type: DataTypes.DATE,    allowNull: false,defaultValue: DataTypes.NOW},        
         cierre:      {type: DataTypes.DATE,    allowNull: false,defaultValue: DataTypes.NOW},
     },
