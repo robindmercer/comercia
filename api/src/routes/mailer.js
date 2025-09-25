fccb890dad88553c717e87b99c162c91a66477ba
robindmercer <robindmercer@gmail.com>
2025-09-25 18:19:48 -0300

    Dotenv Variables

--- api/src/routes/mailer.js
+++ api/src/routes/mailer.js
@@ -1,24 +1,23 @@
-require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const { Router } = require("express");
+const nodemailer = require("nodemailer");
 const router = Router();
 
-const nodemailer = require("nodemailer");
 const { SMTP_HOST, SMTP_USER, SMTP_PORT, SMTP_SECURE, SMTP_PASS } = process.env;
 router.post("/", async (req, res) => {
    const { desde,recibe,email,asunto, texto } = req.body;
-   console.log('process.env.SMTP_HOST: ', process.env.SMTP_HOST);
-   console.log('process.env.SMTP_USER: ', process.env.SMTP_USER);
+   console.log('SMTP_HOST: ', SMTP_HOST);
+   console.log('SMTP_USER: ', SMTP_USER);
 
    // Configura tu transporte SMTP
    const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST, 
-      port: process.env.SMTP_PORT ,
-      secure: process.env.SMTP_SECURE,
+      host: SMTP_HOST, 
+      port: SMTP_PORT ,
+      secure: SMTP_SECURE,
       auth: {
-          user: process.env.SMTP_USER,
-          pass: process.env.SMTP_PASS,
+          user: SMTP_USER,
+          pass: SMTP_PASS,
       },
       tls: {
          rejectUnauthorized: false, // <--- Agrega esta línea
